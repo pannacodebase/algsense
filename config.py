59b552f8a5f9ee1d5e56d5bfd2fd8528e29a115c
Panna <156357260+pannacodebase@users.noreply.github.com>
2024-11-04 19:00:02 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # config.py
 
 # API keys for exclusive access
-ARIA_API_KEY = "4ff5f6c7e9dd48a2acdadc7ebf6148fb"
-ALLEGRO_API_KEY = "f6979747d049447197699bec7cdc579b"
+ARIA_API_KEY = ""
+ALLEGRO_API_KEY = ""
 DATABASE_BASE_URL = "sqlitecloud://ccg8bpswnk.sqlite.cloud:8860/chinook.sqlite"
 # API Key for the database connection
-API_KEY = "ILyD99vjoFZTVZtheYjJbiGVnnBIsxrINb7CjixpQGA"
-SERPER_API_KEY = "a3515e9ba0c3d953399baf732e9a965d65f3d285"
-IMGBB_API_KEY = '77a1ea2630c7d58af94add227ced7180'  # Your Imgbb API key
-DEEPGRAM_API_KEY = 'eab7bf4c5b8f491e9b93ab34313598c4a3b71392'
+API_KEY = ""
+SERPER_API_KEY = ""
+IMGBB_API_KEY = ''  # Your Imgbb API key
+DEEPGRAM_API_KEY = ''
